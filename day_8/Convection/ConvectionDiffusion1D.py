1e581f4060e03c67b8516e96b2d1e31b526947fc
Selvaraj <selvankl@ymail.com>
2022-07-28 09:37:03 -0600

    conv

--- day_8/Convection/ConvectionDiffusion1D.py
+++ day_8/Convection/ConvectionDiffusion1D.py
@@ -18,10 +18,11 @@ __email__ = 'jvilap@mit.edu'
 import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
-%matplotlib qt
+#%matplotlib qt
 plt.close()
 import matplotlib.animation as animation
 
+order = 1;
 "Flow parameters"
 nu = 0.01
 c = 2
@@ -31,23 +32,53 @@ N = 128
 Dx = 1/N
 x = np.linspace(0,1,N+1)
 
-"System matrix and RHS term"
-"Diffusion term"
-Diff = nu*(1/Dx**2)*(2*np.diag(np.ones(N-1)) - np.diag(np.ones(N-2),-1) - np.diag(np.ones(N-2),1))
-"Advection term: centered differences"
-Advp = -0.5*c*np.diag(np.ones(N-2),-1)
-Advm = -0.5*c*np.diag(np.ones(N-2),1)
-Adv = (1/Dx)*(Advp-Advm)
-A = Diff + Adv
-"Source term"
-F = np.ones(N-1)
+"Temporal parameters"
+dt = 0.5
+time = np.arange(0, 3+dt, dt)
+nt = len(time)
 
+"Initialize solution variable"
+U = np.zeros((N-1, nt))
 
+for it in range(nt-1):
+    
+    "System matrix and RHS term"
+    "Diffusion term"
+    Diff = nu*(1/Dx**2)*(2*np.diag(np.ones(N-1)) - np.diag(np.ones(N-2),-1) - np.diag(np.ones(N-2),1))
+    "Upwind scheme"
+    
+    if order<2:
+        cp = max([c,0])
+        cm = min([c,0])
+        "Advection term: centered differences"
+        Advp = cp*(np.diag(np.ones(N-1)) - np.diag(np.ones(N-2), -1))
+        Advm = cm*(np.diag(np.ones(N-1)) - np.diag(np.ones(N-2), 1))
+    else:
+        "Advection term: centered differences"
+        Advp = -0.5*c*np.diag(np.ones(N-2),-1)
+        Advm = -0.5*c*np.diag(np.ones(N-2),1)
+        
+    Adv = (1/Dx)*(Advp-Advm)
+    A = Diff + Adv
+    "Source term"
+    F = np.ones(N-1)
+    
+    "Temporal changes"
+    A = A + (1/dt)*np.diag(np.ones(N-1))
+    F = F + U[:,it] 
+    
+    "Solution of the linear system AU=F"
+    u = np.linalg.solve(A,F)
+    U[:,it] = u
+    
+ua = (1/c)*(x-((1-np.exp(c*x/nu))/(1-np.exp(c/nu))))
+"""
 "Solution of the linear system AU=F"
 U = np.linalg.solve(A,F)
 u = np.concatenate(([0],U,[0]))
 ua = (1/c)*(x-((1-np.exp(c*x/nu))/(1-np.exp(c/nu))))
 
+fig = plt.figure()
 plt.plot(x,ua,'-r',linewidth=2,label='$u_a$')
 plt.plot(x,u,':ob',linewidth=2,label='$\widehat{u}$')
 plt.legend(fontsize=12,loc='upper left')
@@ -55,8 +86,27 @@ plt.grid()
 plt.axis([0, 1,0, 2/c])
 plt.xlabel("x",fontsize=16)
 plt.ylabel("u",fontsize=16)
+"""
+"Animation of the results"
+fig = plt.figure()
+ax = plt.axes(xlim =(0, 1),ylim =(0,1/c)) 
+plt.plot(x,ua,'-r',linewidth=2,label='$u_a$')
+myAnimation, = ax.plot([], [],':ob',linewidth=2)
+plt.grid()
+plt.xlabel("x",fontsize=16)
+plt.ylabel("u",fontsize=16)
 
 
+def animate(i):
+    
+    u = np.concatenate(([0],U[0:N+1,i],[0]))
+    plt.plot(x,u)
+    myAnimation.set_data(x, u)
+    return myAnimation,
+
+anim = animation.FuncAnimation(fig,animate,frames=range(1,nt),blit=True,repeat=False)
+
+"""
 "Compute error"
 error = np.max(np.abs(u-ua))
 print("Linf error u: %g\n" % error)
@@ -65,4 +115,4 @@ print("Linf error u: %g\n" % error)
 P = np.abs(c*Dx/nu)
 print("Pe number Pe=%g\n" % P);
 
-
+"""
