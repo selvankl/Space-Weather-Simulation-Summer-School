9c96838cbb0bb349a45610fa43cf67d69253b4ee
Selvaraj <selvankl@ymail.com>
2022-07-28 09:38:15 -0600

    pollut

--- day_8/Convection/PollutantTransport1D.py
+++ day_8/Convection/PollutantTransport1D.py
@@ -25,12 +25,10 @@ __email__ = 'jvilap@mit.edu'
 import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
-%matplotlib qt
-plt.close()
 import matplotlib.animation as animation
 
 "Flow parameters"
-nu = 0.01
+nu = 0.001
 c = -2
 u0 = 0
 
@@ -43,6 +41,7 @@ Dx = 1/N
 x = np.linspace(0,1,N+1)
 xN = np.concatenate(([x[0]-Dx],x))
 
+order = 2;
 
 "Time parameters"
 dt = 1/50
@@ -50,71 +49,60 @@ time = np.arange(0,3+dt,dt)
 nt = np.size(time)
 
 "Initialize solution variable"
-U = np.zeros((N+1,nt))
+# Specify the order of the approximation
+order = 2
 
+U = np.zeros((N+1,nt))
 
 for it in range(nt-1):
 
     "System matrix and RHS term"
     "Diffusion term"
-    Diff = nu*(1/Dx**2)*(2*np.diag(np.ones(N+1)) - np.diag(np.ones(N),-1) - np.diag(np.ones(N),1))
+    Diff = nu*(1/Dx**2)*(2*np.diag(np.ones(N+2)) - np.diag(np.ones(N+1),-1) - np.diag(np.ones(N+1),1))
 
     "Advection term:"
-        
-    "Sensor"
-    U0 = U[:,it]
-    uaux = np.concatenate(([U0[0]], U0,[U0[N]]))
-    Du = uaux[1:N+3] - uaux[0:N+2] + 1e-8
-    r = Du[0:N+1]/Du[1:N+2]
+    U0 = U[:, it]
     
-    "Limiter"
-    if beta>0:
-        phi = np.minimum(np.minimum(beta*r,1),np.minimum(r,beta))
-        phi = np.maximum(0,phi)
-    else:
-        phi = 2*r/(r**2 + 1)
+    if order ==2:
+        xN = np.concatenate((x, [x[N]+Dx]))
+        "Upwind scheme in 2nd order"
+        cp = max([c,0])
+        cm = min([c,0])
+        "Advection term: centered differences"
+        Advp = cp*((3/2)*np.diag(np.ones(N+2)) - 2*np.diag(np.ones(N+1), -1) + (1/2)*np.diag(np.ones(N), -2))
+        Advm = cm*((3/2)*np.diag(np.ones(N+2)) - 2*np.diag(np.ones(N+1), 1) + (1/2)*np.diag(np.ones(N), 2))
+    elif order==1:
+        "Upwind scheme in first"
+        cp = max([c,0])
+        cm = min([c,0])
+        "Advection term: centered differences"
+        Advp = cp*(np.diag(np.ones(N+1)) - np.diag(np.ones(N), -1))
+        Advm = cm*(np.diag(np.ones(N+1)) - np.diag(np.ones(N), 1))
         
-    phim = phi[0:N]
-    phip = phi[1:N+1]
-        
-    
-    "Upwind scheme"
-    cp = np.max([c,0])
-    cm = np.min([c,0])
-    
-    Advp = cp*(np.diag(1-phi) - np.diag(1-phip,-1))
-    Advm = cm*(np.diag(1-phi) - np.diag(1-phim,1))
-    Alow = Advp-Advm
-    
-    "Centered differences"
-    Advp = -0.5*c*np.diag(phip,-1)
-    Advm = -0.5*c*np.diag(phim,1)
-    Ahigh = Advp-Advm
-        
-    Adv = (1/Dx)*(Ahigh + Alow)
+    Adv = (1/Dx)*(Advp-Advm)
     A = Diff + Adv
     
     "Source term"
     sine = np.sin(2*pi*time[it+1])
     sineplus = 0.5*(sine + np.abs(sine))
-    F = 100*np.exp(-((x-0.8)/0.01)**2)*sineplus
+    F = 100*np.exp(-((xN-0.8)/0.01)**2)*sineplus
     
     "Temporal terms"
-    A = A + (1/dt)*np.diag(np.ones(N+1))
-    F = F + U0/dt
+    A = A + (1/dt)*np.diag(np.ones(N+2))
+    F = F + np.concatenate((U0, [0]))/dt
 
     "Boundary condition at x=0"
-    A[0,:] = (1/Dx)*np.concatenate(([1.5, -2, 0.5],np.zeros(N-2)))
-    F[0] = 0
+    A[0,:] = (1/Dx)*np.concatenate(([1.5, -2, 0.5],np.zeros(N-1))) #left-side
+    F[0] = 0 #left side
 
     "Boundary condition at x=1"
-    A[N,:] = np.concatenate((np.zeros(N),[1]))
-    F[N] = u0
+    A[N+1,:] = (1/Dx**2)*np.concatenate(([1, -2, 1],np.zeros(N-1))) #left-side
+    F[N+1] = 0
 
 
     "Solution of the linear system AU=F"
     u = np.linalg.solve(A,F)
-    U[:,it+1] = u
+    U[:,it+1] = u[:-1]
     u = u[0:N+1]
 
 
